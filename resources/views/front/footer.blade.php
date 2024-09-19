81181e4e669f4a9d3ee53e3d4cb7becf662b44bc
MohammadIndraA <akunpertamasaya5@gmail.com>
2024-09-19 16:07:26 +0700

    update: name

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="footer section">
     <div class="footer__container container grid">
         <div class="footer__content">
-            <a href="#" class="footer__logo">PENDURA</a>
+            <a href="#" class="footer__logo">SIPENDURA</a>
             <p class="footer__description">Cek udara di sekitar<br> andra terlebih dahulu</p>
         </div>
 
